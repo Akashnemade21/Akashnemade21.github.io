2975b448038da1474f238851400ed2d0ccb5c8d1
AkashNemade21 <akashnemade21@gmail.com>
2021-09-06 17:53:09 +0530

    Demo

---
+++ precache-manifest.b7448d8335e423d677ab6cd882f9b2f6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89f29fcc1dae50c585fcece881b7726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06cec57684c55d6a354",
+    "url": "/static/css/2.c91069f5.chunk.css"
+  },
+  {
+    "revision": "915b57b0d2f8327e2654",
+    "url": "/static/css/main.e74c59de.chunk.css"
+  },
+  {
+    "revision": "d06cec57684c55d6a354",
+    "url": "/static/js/2.31790e19.chunk.js"
+  },
+  {
+    "revision": "ba4bbdb8babb46ca88a653f36b75bcea",
+    "url": "/static/js/2.31790e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915b57b0d2f8327e2654",
+    "url": "/static/js/main.0c12abf2.chunk.js"
+  },
+  {
+    "revision": "b8a0c166f9770c07f95d",
+    "url": "/static/js/runtime-main.06ec6c34.js"
+  },
+  {
+    "revision": "f3ce00e3b63422cefc0698a3a1da9ac6",
+    "url": "/static/media/Gilroy-Extrabold.f3ce00e3.woff2"
+  },
+  {
+    "revision": "08e61b50e35df3a7f98d43f8c2701c97",
+    "url": "/static/media/Group 15.08e61b50.svg"
+  },
+  {
+    "revision": "84d43c46397b08e896e039e6005f4bb4",
+    "url": "/static/media/Group 213.84d43c46.svg"
+  },
+  {
+    "revision": "e07be0345675f7a9100d7e1f7c7d1656",
+    "url": "/static/media/Group 214.e07be034.svg"
+  },
+  {
+    "revision": "fc2a1b7dd6a36e1881739bedb6430752",
+    "url": "/static/media/Help-img-1.fc2a1b7d.svg"
+  },
+  {
+    "revision": "58ee9c9c5db18f870ceedd4b56b60926",
+    "url": "/static/media/Help-img-2.58ee9c9c.svg"
+  },
+  {
+    "revision": "0a2502571d91b89e0006cc47c1547636",
+    "url": "/static/media/Help-img-3.0a250257.svg"
+  },
+  {
+    "revision": "983bd114d3da6a0bf64862503bf44d0c",
+    "url": "/static/media/Home-img.983bd114.png"
+  },
+  {
+    "revision": "5313a1b92bf4e0ddfa94526d3b78c9fd",
+    "url": "/static/media/Image 20.5313a1b9.png"
+  },
+  {
+    "revision": "e52fba7ccf6fc65a579ccf48b891fb0b",
+    "url": "/static/media/Mobile 00.e52fba7c.svg"
+  },
+  {
+    "revision": "1afe670fee6a4878270e942349643081",
+    "url": "/static/media/Mobile 01.1afe670f.svg"
+  },
+  {
+    "revision": "8954c53636405b7b01351e630f89ddd4",
+    "url": "/static/media/Mobile 02.8954c536.svg"
+  },
+  {
+    "revision": "fa4ca75f785a471896235111c87ff581",
+    "url": "/static/media/Mobile 10.fa4ca75f.svg"
+  },
+  {
+    "revision": "ce19cd350a05fbec4aeb7c02f348a127",
+    "url": "/static/media/Mobile 20.ce19cd35.svg"
+  },
+  {
+    "revision": "865d75eb8b5c6a9a8c42a8e0d196a457",
+    "url": "/static/media/Test-img.865d75eb.svg"
+  },
+  {
+    "revision": "f53a82711c875daf31e472ddaeddcc61",
+    "url": "/static/media/double-quotes-end.f53a8271.svg"
+  },
+  {
+    "revision": "76f4275791031db68784de2dad585ec3",
+    "url": "/static/media/double-quotes-start.76f42757.svg"
+  },
+  {
+    "revision": "477e75d60071601421f5bf55a98888f2",
+    "url": "/static/media/location.477e75d6.svg"
+  },
+  {
+    "revision": "843eb7ca612d9583ee4b0074541bafc0",
+    "url": "/static/media/mail.843eb7ca.svg"
+  },
+  {
+    "revision": "5bab3fa360ed480ccdfe070eb3748361",
+    "url": "/static/media/mobile.5bab3fa3.svg"
+  },
+  {
+    "revision": "f3c23c743ac7193942aad39e682bb769",
+    "url": "/static/media/notified-form-img.f3c23c74.png"
+  },
+  {
+    "revision": "250836c2bbd2a2626ecd861e24ec048e",
+    "url": "/static/media/notified-img.250836c2.svg"
+  },
+  {
+    "revision": "e32830ed0800748705100fe5337c6f47",
+    "url": "/static/media/reallos-logo-r.e32830ed.svg"
+  },
+  {
+    "revision": "a7aa04eb178f5a7c2c8fac92e52ca778",
+    "url": "/static/media/reallos-logo-white.a7aa04eb.svg"
+  }
+]);
